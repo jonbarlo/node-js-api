95b475aeacfbb0ae2349efdcb1fb6b02c564f5dc
Jonathan Barquero <jonbarlo@hotmail.com>
2025-06-27 02:34:21 -0600

    Removing debug message

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,6 @@ logger(`- DB_USERNAME: ${process.env.DB_USERNAME}`);
 logger(`- DB_NAME: ${process.env.DB_NAME}`);
 logger(`- DB_HOST: ${process.env.DB_HOST}`);
 logger(`- DB_PORT: ${process.env.DB_PORT}`);
-logger(`- DB_PASSWORD: ${process.env.DB_PASSWORD}`);
 logger(`- DB_URL: ${process.env.DB_URL}`);
 
 logger(`- APP_NAME: ${process.env.APP_NAME}`);
